ce5b34f91ff264275861a65fa5748df50e8095ab
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-10-23 20:52:14 -0300

    add list of products

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food.jpg'),
-              Text('Food Paradise')
-            ],
+          appBar: AppBar(
+            title: Text('EasyList'),
           ),
-        ),
-      ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Advance Food Tester');
+                    });
+                  },
+                  child: Text('Add Product'),
+                ),
+              ),
+              Column(
+                children: _products
+                    .map((element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/food.jpg'),
+                              Text(element)
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              )
+            ],
+          )),
     );
   }
 }
