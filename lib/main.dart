45b2e19ee9020e379a22529a22f7b809d7cc94d2
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-10-23 21:43:39 -0300

    add named parameters

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(
+            primarySwatch: Colors.deepOrange,
+            accentColor: Colors.deepPurple,
+            brightness: Brightness.light),
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('EasyList'),
-      ),
-      body: ProductManager('Food Tester'),
-    ));
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: ProductManager(startingProduct: 'Food Tester'),
+        ));
   }
 }
