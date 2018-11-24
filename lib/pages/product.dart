f501e7ce1f484614aa5d96d91174438bb8b93c88
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-11-24 14:24:23 -0300

    fix style product page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -4,18 +4,28 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Detail'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Text('Details!'),
-          RaisedButton(
-            child: Text('BACK'),
-            onPressed: () => Navigator.pop(context),
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Product Detail'),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Details!'),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('BACK'),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
