e9e6be4f0abbea7dfaab63f285695504d43eb424
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-11-24 14:17:47 -0300

    add product page

--- lib/products.dart
+++ lib/products.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
+import './pages/product.dart';
+
 class Products extends StatelessWidget {
   // never change just initialized
   final List<String> products;
-  
+
   // Constructor
   Products(this.products) {
     print('[Products Widget] Constructor');
@@ -11,17 +13,33 @@ class Products extends StatelessWidget {
 
   Widget _buildProductItem(BuildContext context, int index) {
     return Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text(products[index])
-                  ],
-                ),
-              );
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Details'),
+                onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => ProductPage(),
+                      ),
+                    ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 
   Widget _buildProductList() {
-    Widget productCard = Center(child: Text('No products found, please add some'),);
+    Widget productCard = Center(
+      child: Text('No products found, please add some'),
+    );
     if (products.length > 0) {
       productCard = ListView.builder(
         itemBuilder: _buildProductItem,
