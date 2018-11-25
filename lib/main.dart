371d871b1316dba5b3be54a01dcf6f209128e313
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-11-25 16:43:43 -0300

    add tabs to products admin page

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          primarySwatch: Colors.deepOrange,
+          primarySwatch: Colors.green,
           accentColor: Colors.deepPurple,
           brightness: Brightness.light),
       home: AuthPage(),
