b857fa5b8d03e03dc98e09427d4502bec211828a
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-11-25 23:23:29 -0300

    add button for create a product

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -14,42 +14,43 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          decoration: InputDecoration(
-            hintText: 'Please enter a title'
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+                labelText: 'Product Title', hintText: 'Please enter a title'),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
           ),
-          onChanged: (String value) {
-            setState(() {
-              titleValue = value;
-            });
-          },
-        ),
-        TextField(
-          decoration: InputDecoration(
-            hintText: 'Please enter a description'
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Description', hintText: 'Please enter a description'),
+            maxLines: 4,
+            keyboardType: TextInputType.multiline,
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
           ),
-          maxLines: 4,
-          keyboardType: TextInputType.multiline,
-          onChanged: (String value) {
-            setState(() {
-              descriptionValue = value;
-            });
-          },
-        ),
-        TextField(
-          decoration: InputDecoration(
-            hintText: 'Please enter a price'
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Price', hintText: 'Please enter a price'),
+            keyboardType: TextInputType.number,
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
           ),
-          keyboardType: TextInputType.number,
-          onChanged: (String value) {
-            setState(() {
-              priceValue = double.parse(value);
-            });
-          },
-        ),
-      ],
+          RaisedButton(child: Text('Save'), onPressed: () {
+            
+          },)
+        ],
+      ),
     );
   }
 }
