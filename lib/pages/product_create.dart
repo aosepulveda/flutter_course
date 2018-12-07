b3c8d3ca87d4eda5150a80763de9742601dbc8a7
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-12-06 22:27:15 -0300

    change page after creating a new product

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -53,8 +53,11 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
               });
             },
           ),
+          SizedBox(height: 10.0,),
           RaisedButton(
             child: Text('Save'),
+            color: Theme.of(context).accentColor,
+            textColor: Colors.white,
             onPressed: () {
               final Map<String, dynamic> product = {
                 'title': titleValue,
@@ -63,6 +66,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
                 'image': 'assets/food.jpg'
               };
               widget.addProduct(product);
+              Navigator.pushReplacementNamed(context, '/');
             },
           )
         ],
